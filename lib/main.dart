5a0966f293739a0b4c12214c8742b9815717b07c
Batuhan <batuhan.fidan@ogr.iu.edu.tr>
2022-11-17 17:47:48 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/models/task_model.dart';
-import 'package:habit_tracker/widgets/my_list_tile.dart';
+import 'package:habit_tracker/home_page.dart';
 import 'package:hive_flutter/adapters.dart';
 
-import 'screens/task_editor.dart';
-
-late Box box;
-
 void main() async {
-  await Hive.initFlutter();
-  Hive.registerAdapter<Task>(TaskAdapter());
-  box = await Hive.openBox<Task>("tasks");
-  box.add(Task(
-      title: 'this is the first Task',
-      note: 'this is the first task made with the app',
-      creation_date: DateTime.now()));
   runApp(const MyApp());
 }
 
@@ -28,78 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Text(
-          'My Habit App',
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-      ),
-      body: ValueListenableBuilder<Box<Task>>(
-        valueListenable: Hive.box<Task>('tasks').listenable(),
-        builder: (context, box, _) {
-          return Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Today's Task",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  formatDate(DateTime.now(), [d, ",", M, "", yyyy]),
-                  style: TextStyle(
-                    color: Colors.grey.shade700,
-                    fontSize: 18,
-                  ),
-                ),
-                Divider(
-                  height: 40,
-                  thickness: 1,
-                ),
-                Expanded(child: ListView.builder(itemBuilder: (context, index) {
-                  Task currentTask = box.getAt(index)!;
-                  return MyListTile(currentTask, index);
-                }))
-              ],
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => TaskEditor()));
-        },
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
---
+++ lib/util/habit_tile.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class HabitTile extends StatelessWidget {
+  final String habitName;
+
+  final VoidCallback onTap;
+  final VoidCallback settingsTapped;
+  final int timeSpent;
+  final int timeGoal;
+  final bool habitStarted;
+
+  const HabitTile({
+    required this.habitName,
+    required this.onTap,
+    required this.settingsTapped,
+    required this.timeSpent,
+    required this.timeGoal,
+    required this.habitStarted,
+    super.key,
+  });
+
+  String formatToMinSec(int totalSeconds) {
+    String secs = (totalSeconds % 60).toString();
+    String mins = (totalSeconds / 60).toStringAsFixed(1);
+
+    if (secs.length == 1) {
+      secs = '0' + secs;
+    }
+
+    if (mins[1] == '.') {
+      mins = mins.substring(0, 1);
+    }
+
+    return mins + ':' + secs;
+  }
+
+  double percentCompleted() {
+    return timeSpent / (timeGoal * 60);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20, top: 20),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: onTap,
+                  child: SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: Stack(children: [
+                      CircularPercentIndicator(
+                        radius: 30,
+                        percent:
+                            percentCompleted() < 1 ? percentCompleted() : 1,
+                        progressColor: percentCompleted() > 0.5
+                            ? (percentCompleted() > 0.75
+                                ? Colors.red
+                                : Colors.orange)
+                            : Colors.green,
+                      ),
+                      Center(
+                        child:
+                            Icon(habitStarted ? Icons.pause : Icons.play_arrow),
+                      ),
+                    ]),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      habitName,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      formatToMinSec(timeSpent) +
+                          '/' +
+                          timeGoal.toString() +
+                          '=' +
+                          (percentCompleted() * 100).toStringAsFixed(0) +
+                          '%',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: settingsTapped,
+              child: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
